43a539ac922670cec8c05f00ae65c240f711dfb2
Daniel Roschmann <droschmann01@gmail.com>
2026-06-03 13:26:49 +0200

    get specific movie route

--- Server/MovieStreamServer/main.go
+++ Server/MovieStreamServer/main.go
@@ -9,11 +9,8 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello")
-	})
-
 	router.GET("/movies", controller.GetMovies())
+	router.GET("/movie/:imdb_id", controller.GetMovie())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)

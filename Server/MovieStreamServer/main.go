b6bc9a752e3ed49e987cb798bc855ad57d9b0089
Daniel Roschmann <droschmann01@gmail.com>
2026-06-06 16:28:25 +0200

    cors

--- Server/MovieStreamServer/main.go
+++ Server/MovieStreamServer/main.go
@@ -7,12 +7,20 @@ import (
 
 	"github.com/danielroschmann/Movie-Streaming-Recommendation/Server/MovieStreamServer/database"
 	"github.com/danielroschmann/Movie-Streaming-Recommendation/Server/MovieStreamServer/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	var client *mongo.Client = database.Connect()
 
